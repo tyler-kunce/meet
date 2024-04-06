f3eac5c38669a64ddcb49e02b67867f70be440f4
tyler-kunce <tyler.kunce@gmail.com>
2024-04-06 08:28:39 -0700

    Integration testing and Google API integration

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 // src/api.js
 
+import { toBeChecked } from '@testing-library/jest-dom/matchers';
 import mockData from './mock-data';
 
 /**
@@ -22,6 +23,81 @@ export const extractLocations = (events) => {
  * This function will fetch the list of all events
  */
 
-export const getEvents = async () => {
-  return mockData;
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
 };
+
+const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
+  } else {
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
+  }
+};
+
+export const getEvents = async () => {
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      'https://ossi5ono6i.execute-api.us-east-2.amazonaws.com/dev/api/get-events' +
+      '/' +
+      token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null;
+  }
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const response = await fetch(
+        'https://ossi5ono6i.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getAccessToken(code);
+  }
+  return accessToken;
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    'https://ossi5ono6i.execute-api.us-east-2.amazonaws.com/dev/api/token' +
+      '/' +
+      encodeCode
+  );
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};
+ß;
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,11 +1,15 @@
 // src/components/CitySearch.js
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
@@ -23,6 +27,7 @@ const CitySearch = ({ allLocations }) => {
     const value = event.target.textContent;
     setQuery(value);
     setShowSuggestions(false); // to hide the list
+    setCurrentCity(value);
   };
 
   return (
