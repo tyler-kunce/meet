87832a25186d41f2aff4e144b116aee5a7f2bf4c
tyler-kunce <tyler.kunce@gmail.com>
2024-05-08 14:06:06 -0500

    Debug

--- src/api.js
+++ src/api.js
@@ -37,10 +37,9 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem('lastEvents');
-    NProgress.done();
     return events?JSON.parse(events):[];
   }
-  
+
   const token = await getAccessToken();
 
   if (token) {
@@ -52,7 +51,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
